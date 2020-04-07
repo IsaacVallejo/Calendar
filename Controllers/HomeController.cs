00571beebfa3cda213dcd3743f08d7cc18d8244b
Isaac Vallejo <isaac.vallejo90@gmail.com>
2020-04-06 17:04:06 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (EventDBEntities db = new EventDBEntities())
+            {
+                var events = db.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
